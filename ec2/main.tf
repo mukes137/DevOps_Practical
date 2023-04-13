732923df115dd980c4bf9876d1794be1f8839d7f
mukes137 <t4.cs.soft9@gmail.com>
2023-04-13 12:51:03 +0545

    pushing code for CICD

---
+++ ec2/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.56.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}  
+
+resource "aws_security_group" "ec2-sg"{
+  name = "${var.name}-sg"
+  description = "EC2 Security Group"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "sg-http"{
+  type = "ingress"
+  security_group_id = aws_security_group.ec2-sg.id
+  from_port        = 80
+  to_port          = 80
+  protocol         = "TCP"
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg-https"{
+  type = "ingress"
+  security_group_id = aws_security_group.ec2-sg.id
+  from_port        = 443
+  to_port          = 443
+  protocol         = "TCP"
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg-ssh"{
+  type = "ingress"
+  security_group_id = aws_security_group.ec2-sg.id
+  from_port        = 22
+  to_port          = 22
+  protocol         = "TCP"
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg-egress"{
+  type = "egress"
+  security_group_id = aws_security_group.ec2-sg.id
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_instance" "server" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  key_name = "mukesh"
+    root_block_device{
+    volume_size = 8
+    volume_type = "gp3"
+    encrypted = true
+  }
+  tags = merge(
+  var.tags,
+  {
+    Name              = var.name
+    Environment       = "test"
+    TTL               = "Apr 12"
+  }
+)
+}
