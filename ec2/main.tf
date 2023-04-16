025776e732099e56bd36014fd3f2c9476b5da9d6
mukes137 <t4.cs.soft9@gmail.com>
2023-04-16 16:37:49 +0545

    changes in main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -9,9 +9,10 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  region     = "us-east-1"
   access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
   secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
-  region     = "us-east-1"
+  
 }
 
 
