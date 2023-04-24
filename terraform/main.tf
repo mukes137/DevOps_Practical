81cd58166b3966bc99bd0822ebd14fe7a6bdb69c
mukes137 <t4.cs.soft9@gmail.com>
2023-04-24 20:47:31 +0545

    changes in structure

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "~> 3.56.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  # profile                     = "default" 
+  shared_credentials_files = ["~/.aws/credentials"]
+
+}
+
+module "ec2" {
+  source = "./terraform/modules/ec2"
+}
+
+module "s3" {
+  source = "./terraform/modules/s3"
+}
